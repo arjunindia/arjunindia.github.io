6ada463f237a4c859cf13fad3710443c4e030bc3
Arjun S <arjun8107@gmail.com>
2023-08-17 21:41:41 +0530

    update projects

---
+++ src/projects.ts
@@ -0,0 +1,75 @@
+interface Projects {
+    name: string;
+    description: string;
+    image: string;
+    url: string;
+  }
+  export const projects: Projects[] = [
+    {
+      name: "Portfolio Page",
+      description: "A Page, for me and my projects.",
+      image: "portfolio.png",
+      url: "https://github.com/arjunindia/arjunindia.github.io",
+    },
+    {
+      name: "TUI Pokedex",
+      description: "A Pokedex in your terminal.",
+      image: "tuipokedex.png",
+      url: "https://github.com/arjunindia/tui_pokedex",
+    },
+    {
+      name: "PixelPlace",
+      description:
+        "Collaborative drawing board where users can draw on a canvas and see the changes made by other users in realtime.",
+      image: "pixelplace.png",
+      url: "https://github.com/arjunindia/Hak-Night",
+    },
+    {
+      name: "Chikku",
+      description: "A discord bot used for giving points as rewards for projects",
+      image: "chikku.png",
+      url: "https://github.com/arjunindia/Chikku-bot",
+    },
+    {
+      name: "Lucia",
+      description: "A multimedia discord bot.",
+      image: "lucia.png",
+      url: "https://github.com/arjunindia/Lucia-bot",
+    },
+    {
+      name: "Fake E-Commerce",
+      description: "A fake E-Commerce app using NextJS 12 SSR.",
+      image: "fake-ecommerce.png",
+      url: "https://github.com/arjunindia/fake-ecommerce-app",
+    },
+    {
+        name: "jant-fr-rep",
+        description:"A replication of https://jant.fr/ with react and plain css.",
+        image: "jant.png",
+        url: "https://github.com/arjunindia/jant-fr-rep",
+    },
+    {
+        name: "React PewPew",
+        description: "A set of components based on iFrames and JSZip for easily rendering PewEngine (embeddable version of the game PewPew Live) in react.",
+        image: "react-pewpew.png",
+        url: "https://github.com/arjunindia/react-pewpew",
+    },
+    {
+        name: "pewpew-quick-repl",
+        description:"A quick prototyping environment in the browser for pewpew levels.",
+        image: "pewpewrepl.png",
+        url: "https://github.com/arjunindia/pewpew-quick-repl",
+    },
+    {
+        name: "CRUD library",
+        description:"A simple library management app built with Svelte to demonstrate CRUD",
+        image: "library.png",
+        url: "https://github.com/arjunindia/simple-CRUD-library-management",
+    },
+    {
+        name: "Voice Controlled Game",
+        description: "A game that can be controlled using voice commands.",
+        image: "voice-controlled-game.png",
+        url: "https://github.com/arjunindia/Voice-Controlled-Game",
+    }
+  ];
